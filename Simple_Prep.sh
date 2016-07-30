8e2f69c3dc422c8ca3c1f194822882f8e28943f3
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2016-07-30 14:38:57 -0400

    download different miniconda for OSX and Linux

--- Simple_Prep.sh
+++ Simple_Prep.sh
@@ -1,24 +1,22 @@
+#!/bin/bash
 # Setup standalone python environment
-echo "Setup standalone python environment
+echo "Setup standalone python environment"
 
-
-
-"
-wget http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+if [ `uname` == 'Darwin' ]; then
+    wget http://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
+else
+    wget http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+fi
 chmod +x miniconda.sh
 ./miniconda.sh -b
 export PATH=$HOME/miniconda2/bin:$PATH
 conda config --add channels conda-forge
 
 # Get the repo and Create the specific versioned python environment
-echo "Get the repo and Create the specific versioned python environment
+echo "Get the repo and Create the specific versioned python environment"
 
-
-
-"
 git clone https://github.com/satra/simple_workflow.git
 cd simple_workflow
 conda env create -f environment.yml
 source activate bh_demo
 pip install https://github.com/satra/prov/archive/enh/rdf-1.x.zip
-
